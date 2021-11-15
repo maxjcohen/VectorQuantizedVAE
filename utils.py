4b7316f2c2c47795d8492904afe2884cd2905744
Max Cohen <max.zagouri@pm.me>
2021-11-15 05:45:10 -0500

    Add option for returning indexes in compute_logits

--- utils.py
+++ utils.py
@@ -11,13 +11,35 @@ def compute_logits(
     dataloader: iter,
     device_model: torch.device = "cpu",
     device_output: torch.device = "cpu",
+    indexes: bool = False,
 ):
+    """Returns codebook associated with dataset.
+
+    Iterate through the dataset, storing codebook associated with each batch. If
+    `indexes` is set to `True`, only store the `argmin` instead of the logits.
+
+    Parameters
+    ==========
+    model: VQVAE model
+    dataloader: iterator over the dataset.
+    device_model: device for storing inputs. Default is `"cpu"`.
+    device_output: device for storing the codebooks. Because the size of the tensor can
+    be considerably big, it is advised to stick to `"cpu"`. Default is `"cpu"`.
+    indexes: if `True`, stores the `argmin` of the logits. Default is `False`.
+
+    Returns
+    =======
+    Codebooks tensor with shape `(N, W, H, M)` if `indexes` is set to `False`, otherwise
+    `(N, W, H)`. `N` is the number of samples in the dataset, `W` and `H` are the
+    dimensions of the feature map, `M` is the number of codebooks available.
+    """
     logits = []
     for images, _ in tqdm.tqdm(dataloader, total=len(dataloader)):
         images = images.to(device_model)
         model(images)
-        logits.append(
-            torch.argmin(model.codebook.distances[0], dim=-1).to(device_output)
-        )
+        logit = model.codebook.distances[0].to(device_output)
+        if indexes:
+            logit = toch.argmin(logit, dim=-1)
+        logits.append(logit)
 
     return torch.cat(logits, dim=0)
